a0bb7ed6ad4bb5227ce1d283ccf5a1d2c34021f0
admin <admin@mail.ru>
2026-07-30 15:21:27 +0300

    Чистка

--- src/WpfApp1/ViewModels/MainWindowViewModel.cs
+++ src/WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -6,25 +6,11 @@ namespace WpfApp1.ViewModels;
 
 public class MainWindowViewModel : Base.ViewModelBase
 {
-    public string Name
-    {
-        get;
-        init => Set(ref field, value);
-    } = string.Empty;
-
     public string Title
     {
         get;
         init => Set(ref field, value);
     } = "Опытное приложение";
-
-    public ICommand HelloCommand => field ??=
-        new LambdaCommand(OnHelloCommandExecuted);
-    private void OnHelloCommandExecuted(object? p)
-    {
-        var message = $"Привет, {Name}!";
-        MessageBox.Show(message, "Приветствие");
-    }
 
     /// <summary>
     /// Закрыть приложение
